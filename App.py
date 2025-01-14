972f05cf9e2dc5d500e5333cba7f4f075330fc4c
shreyapawar07 <shreyapawark7@gmail.com>
2025-01-14 22:20:59 +0530

    Adding all the data

--- App.py
+++ App.py
@@ -1,10 +1,29 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,jsonify
 
 app = Flask(__name__)
 
+users = [
+    {
+    "name":"Shreya",
+    "Age":23
+},
+  {
+    "name":"Priya",
+    "Age":21
+},
+  {
+    "name":"samiksha",
+    "Age":22
+}
+]
+
 @app.route("/")
 def home():
-    return render_template('Homepage.html')
+    return render_template('Homepage.html',users=users)
+
+@app.route("/api/jobs")
+def Jobs():
+    return jsonify(users)
 
 
 if __name__ == '__main__':
